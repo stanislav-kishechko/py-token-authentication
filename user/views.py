aedc9f151e550fd3aa4f072321a4ea8aa003c824
Stanislav Kishechko <skishechko@gmail.com>
2026-01-04 14:58:42 +0200

    The blank line in the end

--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
-from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.settings import api_settings
+from rest_framework import generics
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny
+)
 
 from user.serializers import UserSerializer
 
